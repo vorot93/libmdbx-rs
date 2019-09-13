8fc50a8f749a8133628c332eac38e807ad10d1c2
Victor Porof <vporof@mozilla.com>
2019-09-13 19:30:02 +0200

    Use all compiler flags from the makefile when building (#64)
    Signed-off-by: Victor Porof <victor.porof@gmail.com>

--- lmdb-sys/build.rs
+++ lmdb-sys/build.rs
@@ -43,7 +43,10 @@ fn main() {
             .define("MDB_IDL_LOGN", Some(MDB_IDL_LOGN.to_string().as_str()))
             .file(lmdb.join("mdb.c"))
             .file(lmdb.join("midl.c"))
+            // https://github.com/mozilla/lmdb/blob/b7df2cac50fb41e8bd16aab4cc5fd167be9e032a/libraries/liblmdb/Makefile#L23
             .flag_if_supported("-Wno-unused-parameter")
+            .flag_if_supported("-Wbad-function-cast")
+            .flag_if_supported("-Wuninitialized")
             .compile("liblmdb.a")
     }
 }
