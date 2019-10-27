5f5245ad4d7cd59388cb08577daad92f24a946c2
Victor Porof <victor.porof@gmail.com>
2019-10-27 05:37:58 +0100

    Bump version to 0.9.6
    Signed-off-by: Victor Porof <victor.porof@gmail.com>

--- lmdb-sys/src/lib.rs
+++ lmdb-sys/src/lib.rs
@@ -1,7 +1,7 @@
 #![deny(warnings)]
 #![allow(non_camel_case_types)]
 #![allow(clippy::all)]
-#![doc(html_root_url = "https://docs.rs/lmdb-rkv-sys/0.9.5")]
+#![doc(html_root_url = "https://docs.rs/lmdb-rkv-sys/0.9.6")]
 
 extern crate libc;
 
