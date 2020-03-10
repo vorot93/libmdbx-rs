4363ec04260b8f1c7db6b1f5c0e1c59fd009c228
Victor <victor.porof@gmail.com>
2020-03-10 12:38:40 +0100

    Bump version to 0.14.0
    Signed-off-by: Victor <victor.porof@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //! [Lightning Memory-mapped Database (LMDB)](https://symas.com/lmdb).
 
 #![deny(missing_docs)]
-#![doc(html_root_url = "https://docs.rs/lmdb-rkv/0.13.1")]
+#![doc(html_root_url = "https://docs.rs/lmdb-rkv/0.14.0")]
 
 extern crate byteorder;
 extern crate libc;
