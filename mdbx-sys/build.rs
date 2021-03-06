bb126d6d38d803c7891404f1ec21100a36b3509f
Artem Vorotnikov <artem@vorotnikov.me>
2021-03-06 04:47:13 +0300

    remove pkg-config

--- mdbx-sys/build.rs
+++ mdbx-sys/build.rs
@@ -11,28 +11,26 @@ fn main() {
     let mut mdbx = PathBuf::from(&env::var("CARGO_MANIFEST_DIR").unwrap());
     mdbx.push("libmdbx");
 
-    if pkg_config::find_library("libmdbx").is_err() {
-        let mut builder = cc::Build::new();
+    let mut builder = cc::Build::new();
 
-        builder
-            .file(mdbx.join("mdbx.c"))
-            .flag_if_supported("-Wno-unused-parameter")
-            .flag_if_supported("-Wbad-function-cast")
-            .flag_if_supported("-Wuninitialized");
+    builder
+        .file(mdbx.join("mdbx.c"))
+        .flag_if_supported("-Wno-unused-parameter")
+        .flag_if_supported("-Wbad-function-cast")
+        .flag_if_supported("-Wuninitialized");
 
-        if env::var("CARGO_FEATURE_WITH_ASAN").is_ok() {
-            builder.flag("-fsanitize=address");
-        }
-
-        if env::var("CARGO_FEATURE_WITH_FUZZER").is_ok() {
-            builder.flag("-fsanitize=fuzzer");
-        } else if env::var("CARGO_FEATURE_WITH_FUZZER_NO_LINK").is_ok() {
-            builder.flag("-fsanitize=fuzzer-no-link");
-        }
-
-        let flags = format!("{:?}", builder.get_compiler().cflags_env());
-        builder.define("MDBX_BUILD_FLAGS", flags.as_str());
-
-        builder.compile("libmdbx.a")
+    if env::var("CARGO_FEATURE_WITH_ASAN").is_ok() {
+        builder.flag("-fsanitize=address");
     }
+
+    if env::var("CARGO_FEATURE_WITH_FUZZER").is_ok() {
+        builder.flag("-fsanitize=fuzzer");
+    } else if env::var("CARGO_FEATURE_WITH_FUZZER_NO_LINK").is_ok() {
+        builder.flag("-fsanitize=fuzzer-no-link");
+    }
+
+    let flags = format!("{:?}", builder.get_compiler().cflags_env());
+    builder.define("MDBX_BUILD_FLAGS", flags.as_str());
+
+    builder.compile("libmdbx.a")
 }
