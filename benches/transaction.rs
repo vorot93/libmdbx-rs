93ad36c5f12f2f1c6d5698e35147cc8cf36c2d5f
Artem Vorotnikov <artem@vorotnikov.me>
2021-09-10 18:51:21 +0300

    Run CI on stable, port benches to Criterion

--- benches/transaction.rs
+++ benches/transaction.rs
@@ -1,19 +1,15 @@
-#![feature(test)]
-extern crate test;
-
 mod utils;
 
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use ffi::*;
 use libc::size_t;
 use mdbx::WriteFlags;
 use rand::{prelude::SliceRandom, SeedableRng};
 use rand_xorshift::XorShiftRng;
 use std::ptr;
-use test::{black_box, Bencher};
 use utils::*;
 
-#[bench]
-fn bench_get_rand(b: &mut Bencher) {
+fn bench_get_rand(c: &mut Criterion) {
     let n = 100u32;
     let (_dir, env) = setup_bench_db(n);
     let txn = env.begin_ro_txn().unwrap();
@@ -22,17 +18,18 @@ fn bench_get_rand(b: &mut Bencher) {
     let mut keys: Vec<String> = (0..n).map(get_key).collect();
     keys.shuffle(&mut XorShiftRng::from_seed(Default::default()));
 
-    b.iter(|| {
-        let mut i = 0usize;
-        for key in &keys {
-            i += txn.get(&db, key).unwrap().unwrap().len();
-        }
-        black_box(i);
+    c.bench_function("bench_get_rand", |b| {
+        b.iter(|| {
+            let mut i = 0usize;
+            for key in &keys {
+                i += txn.get(&db, key).unwrap().unwrap().len();
+            }
+            black_box(i);
+        })
     });
 }
 
-#[bench]
-fn bench_get_rand_raw(b: &mut Bencher) {
+fn bench_get_rand_raw(c: &mut Criterion) {
     let n = 100u32;
     let (_dir, env) = setup_bench_db(n);
     let _txn = env.begin_ro_txn().unwrap();
@@ -53,22 +50,23 @@ fn bench_get_rand_raw(b: &mut Bencher) {
         iov_base: ptr::null_mut(),
     };
 
-    b.iter(|| unsafe {
-        let mut i: size_t = 0;
-        for key in &keys {
-            key_val.iov_len = key.len() as size_t;
-            key_val.iov_base = key.as_bytes().as_ptr() as *mut _;
+    c.bench_function("bench_get_rand_raw", |b| {
+        b.iter(|| unsafe {
+            let mut i: size_t = 0;
+            for key in &keys {
+                key_val.iov_len = key.len() as size_t;
+                key_val.iov_base = key.as_bytes().as_ptr() as *mut _;
 
-            mdbx_get(txn, dbi, &key_val, &mut data_val);
+                mdbx_get(txn, dbi, &key_val, &mut data_val);
 
-            i += key_val.iov_len;
-        }
-        black_box(i);
+                i += key_val.iov_len;
+            }
+            black_box(i);
+        })
     });
 }
 
-#[bench]
-fn bench_put_rand(b: &mut Bencher) {
+fn bench_put_rand(c: &mut Criterion) {
     let n = 100u32;
     let (_dir, env) = setup_bench_db(0);
 
@@ -80,16 +78,17 @@ fn bench_put_rand(b: &mut Bencher) {
     let mut items: Vec<(String, String)> = (0..n).map(|n| (get_key(n), get_data(n))).collect();
     items.shuffle(&mut XorShiftRng::from_seed(Default::default()));
 
-    b.iter(|| {
-        let txn = env.begin_rw_txn().unwrap();
-        for &(ref key, ref data) in items.iter() {
-            txn.put(&db, key, data, WriteFlags::empty()).unwrap();
-        }
+    c.bench_function("bench_put_rand", |b| {
+        b.iter(|| {
+            let txn = env.begin_rw_txn().unwrap();
+            for &(ref key, ref data) in items.iter() {
+                txn.put(&db, key, data, WriteFlags::empty()).unwrap();
+            }
+        })
     });
 }
 
-#[bench]
-fn bench_put_rand_raw(b: &mut Bencher) {
+fn bench_put_rand_raw(c: &mut Criterion) {
     let n = 100u32;
     let (_dir, _env) = setup_bench_db(0);
 
@@ -108,20 +107,31 @@ fn bench_put_rand_raw(b: &mut Bencher) {
         iov_base: ptr::null_mut(),
     };
 
-    b.iter(|| unsafe {
-        let mut txn: *mut MDBX_txn = ptr::null_mut();
-        mdbx_txn_begin_ex(env, ptr::null_mut(), 0, &mut txn, ptr::null_mut());
+    c.bench_function("bench_put_rand_raw", |b| {
+        b.iter(|| unsafe {
+            let mut txn: *mut MDBX_txn = ptr::null_mut();
+            mdbx_txn_begin_ex(env, ptr::null_mut(), 0, &mut txn, ptr::null_mut());
 
-        let mut i: ::libc::c_int = 0;
-        for &(ref key, ref data) in items.iter() {
-            key_val.iov_len = key.len() as size_t;
-            key_val.iov_base = key.as_bytes().as_ptr() as *mut _;
-            data_val.iov_len = data.len() as size_t;
-            data_val.iov_base = data.as_bytes().as_ptr() as *mut _;
+            let mut i: ::libc::c_int = 0;
+            for &(ref key, ref data) in items.iter() {
+                key_val.iov_len = key.len() as size_t;
+                key_val.iov_base = key.as_bytes().as_ptr() as *mut _;
+                data_val.iov_len = data.len() as size_t;
+                data_val.iov_base = data.as_bytes().as_ptr() as *mut _;
 
-            i += mdbx_put(txn, dbi, &key_val, &mut data_val, 0);
-        }
-        assert_eq!(0, i);
-        mdbx_txn_abort(txn);
+                i += mdbx_put(txn, dbi, &key_val, &mut data_val, 0);
+            }
+            assert_eq!(0, i);
+            mdbx_txn_abort(txn);
+        })
     });
 }
+
+criterion_group!(
+    benches,
+    bench_get_rand,
+    bench_get_rand_raw,
+    bench_put_rand,
+    bench_put_rand_raw
+);
+criterion_main!(benches);
