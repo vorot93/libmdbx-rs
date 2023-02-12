dbff6c0cb51989f19d31d4e13ae62f1425b8c73f
Tim Nieradzik <tim@sparse.tech>
2023-02-12 20:03:37 +0300

    Fix linking with musl
    Resolves the following ld errors:
    
    ```
    mdbx.c:10827: undefined reference to `__cpu_indicator_init'
    mdbx.c:10830: undefined reference to `__cpu_model'
    mdbx.c:10834: undefined reference to `__cpu_model'
    mdbx.c:10838: undefined reference to `__cpu_model'
    ```

--- mdbx-sys/build.rs
+++ mdbx-sys/build.rs
@@ -93,9 +93,17 @@ fn main() {
             .unwrap()
             .trim()
     );
+
+    // __cpu_model is not available in musl
+    let is_musl = env::var("TARGET").unwrap().ends_with("-musl");
+
     cc_builder
         .define("MDBX_BUILD_FLAGS", flags.as_str())
         .define("MDBX_TXN_CHECKOWNER", "0")
+        .define(
+            "MDBX_HAVE_BUILTIN_CPU_SUPPORTS",
+            if is_musl { "0" } else { "1" },
+        )
         .file(mdbx.join("mdbx.c"))
         .compile("libmdbx.a");
 }
